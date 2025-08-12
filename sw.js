c0cc9cf24c4bed8e18a94751d03c5f4fe0f2e6f4
antoniopedroniart-ctrl <antoniopedroni.art@gmail.com>
2025-08-12 04:57:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,26 @@
-const CACHE = 'fenice-v2';
+const CACHE = 'fenice-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './assets/anterior_muscles.png',
+  './assets/posterior_muscles.png',
+  './assets/bench_press.png',
+  './assets/back_squat.png'
 ];
 
 self.addEventListener('install', e=>{
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil((async()=>{
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
 self.addEventListener('fetch', e=>{
   e.respondWith(
